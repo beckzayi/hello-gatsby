ec2813aeedd286a9c08229ac214da9bdbe41373b
Darren Li <dxl@enablehr.com>
2020-03-30 10:07:11 +1100

    footer image svg

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,13 +6,13 @@ const Footer = () => (
             <div className="col">&copy; { new Date().getFullYear() } enableHR &middot; All rights reserved</div>
             <div className="col">
                 <div className="footer-social">
-                    <a href="https://www.facebook.com/enableHRAU/" target="_blank">
-                        <img src="/images/facebook.svg" />
+                    <a href="https://www.facebook.com/enableHRAU/" target="_blank" rel="noopener noreferrer">
+                        <img src="/images/facebook.svg" alt="Facebook" />
                     </a>
-                    <a href="https://www.linkedin.com/company/enablehr" target="_blank">
+                    <a href="https://www.linkedin.com/company/enablehr" target="_blank" rel="noopener noreferrer">
                         <img src="/images/linkedin.svg" alt="Linkedin" />
                     </a>
-                    <a href="https://twitter.com/enableHR" target="_blank">
+                    <a href="https://twitter.com/enableHR" target="_blank" rel="noopener noreferrer">
                         <img src="/images/twitter.svg" alt="Twitter" />
                     </a>
                 </div>
