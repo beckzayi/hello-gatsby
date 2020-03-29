fff72abf952b6703040871dbae25d5a592931481
Darren Li <dxl@enablehr.com>
2020-03-30 00:38:04 +1100

    header nav links hover & current-page

--- src/components/header.js
+++ src/components/header.js
@@ -8,8 +8,10 @@ const Header = () => (
         </a>
         <div className="navbar-nav-scroll">
             <ul className="navbar-nav">
-                <li className="nav-item"><Link to="/" className="nav-link" activeClassName="current-page">Home</Link></li>
+                <li className="nav-item"><Link to="/home" className="nav-link" activeClassName="current-page">Home</Link></li>
+                <li className="nav-item"><Link to="/docs" className="nav-link" activeClassName="current-page">Docs</Link></li>
                 <li className="nav-item"><Link to="/development" className="nav-link" activeClassName="current-page">Development</Link></li>
+                <li className="nav-item"><Link to="/partner" className="nav-link" activeClassName="current-page">Partner</Link></li>
             </ul>
         </div>
     </header>
